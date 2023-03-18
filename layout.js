b2a17b28e4552e20d27101d057b777143512e41c
CJazmer <125234435+CJazmer@users.noreply.github.com>
2023-03-18 16:46:29 -0500

    Dynamic Footer

---
+++ layout.js
@@ -0,0 +1,6 @@
+
+// DYNAMIC FOOTER ----------------------------------
+let xhxixfrtysudg = new Date();
+let footerrrrr = document.getElementsByTagName("footer")[0];
+footerrrrr.innerHTML = "<p>Copyright &copy; "+xhxixfrtysudg.getFullYear()+" - Conner Jasmer</p>";
+footerrrrr.style = "text-align: center; padding: 1em;"; //CSS
