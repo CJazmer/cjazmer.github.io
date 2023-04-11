030282908279fbfce9b9f73209f8fb88eb92776c
CJazmer <125234435+CJazmer@users.noreply.github.com>
2023-04-11 13:59:50 -0500

    Add files via upload

--- databases/animalsdatabase.js
+++ databases/animalsdatabase.js
@@ -15,6 +15,7 @@ const animalsdatabaseother = [
 const animalsdatabasespider = [
     {name: "Harvest Spider", commonness: "very common", family: "", tags: ""},
     {name: "Yellow Garden Spider", commonness: "", family: "", tags: ""},
+    {name: "Wolf Spider", commonness: "", family: "", tags: "", description: "Wolf spiders don't spin webs to catch prey, but rather use their good eyesite to catch prey on the ground"},
 ];
 const animalsdatabasebirds = [
     {name: "Black Capped Chickadee", commonness: "", family: "", tags: ""},
@@ -24,10 +25,10 @@ const animalsdatabasebirds = [
     {name: "Crow", commonness: "common", family: "", tags: ""},
     {name: "Raven", commonness: "", family: "", tags: ""},
     {name: "Eagle", commonness: "very rare", family: "", tags: ""},
-    {name: "Hawk", commonness: "rare", family: "", tags: ""},
+    {name: "Hawk", commonness: "common", family: "", tags: ""},
     {name: "Mallard Duck", commonness: "common", family: "", tags: "water"},
     {name: "Morning Dove", commonness: "common", family: "", tags: ""},
-    {name: "Pelican", commonness: "rare", family: "", tags: "water"},
+    {name: "American White Pelican", commonness: "rare", family: "", tags: "water"},
     {name: "Seagull", commonness: "very common", family: "", tags: "water"},
     {name: "Redwinged Blackbird", commonness: "common", family: "", tags: ""},
     {name: "Robin", commonness: "very common", family: "", tags: ""},
