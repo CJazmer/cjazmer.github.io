51cb28eb2ad086985929e6e24780168c9f7a599a
CJazmer <125234435+CJazmer@users.noreply.github.com>
2023-05-04 15:10:20 -0500

    Teaches you the instruments

--- apps/musicteacher/musicteacherdatabase.js
+++ apps/musicteacher/musicteacherdatabase.js
@@ -1,74 +1,228 @@
 const musicteacherdatabase = [
-    {title: "Drums", thehtml: `
+    {title: "Drums🥁", thehtml: `
         <p>the setup below is the standard setup for right hand players.</p>    
         <img src="./assets/drumslabled.jpg">
-        <h1>Cymbals</h1>
-        <p>4 cymbal types: High-Hat, Crash, Ride, Splash. Leave a gap between the Highhat cymbals so they're not touching when in rest position.</p>
-        <h1>Drum Sticks</h1>
-        <p>Most drums sticks have a number and letter. The number is the stick thickness (7 thinnest, 2 thickest, 5 is middle/default). Letter is the tip shape (A "acorn", B "Barrel")</p>
-    `
-    },
-    {title: "Electric Instruments", thehtml: `
-        <h1>Pickups</h1>
-        <img src="./assets/pickuptypeslabled.jpg">
-        <p>Single Coil, Humbucker, P90</p>
-        <h1>Body Types</h1>
-        <p>acoustic, </p>
-    `
-    },
-    {title: "Mixers", thehtml: `
-        <h1>Analog & Digital</h1>
-        <p></p>
-        <h1>Volume</h1>
+    <h1>Cymbals</h1>
+        <p>4 cymbal types: High-Hat, Crash, Ride, Splash. Leave a gap between the Highhat cymbals so they're not touching when in rest position. </p>
+    <h1>Snare</h1>
+        <p>Use the "throw off" switch to turn on and off the snare. The black circle in the center is called the "power dot" or "batter". It's a heaver part that protects the drum from hard hits and adds a different sound. The "Gel Pads / Moongel" is used to dampen the drum and stop the resonating. Put the gel pads near the edge of your cymbal to cut the ringing</p>
+    <h1>Drum Sticks</h1>
+        <p>Drum StickMost drums sticks have a number and letter. The number is the stick thickness (7 thinnest, 2 thickest, 5 is middle/default). Letter is the tip shape (A "acorn", B "Barrel")</p>
+    <h1>Brushes & Rods</h1>
+        <p>Rods are much quieter than drumsticks and can be used to soften the sound. Brushes can make sweeping sound for generas like jazz.</p>
+    <h1>Most common brands</h1>
         <p></p>
     `
     },
-    {title: "Speakers", thehtml: `
-        <h1>Active & Passive</h1>
-        <p>Active is powered and has plugin or battery. Passive does'nt have power source and needs amplifier.</p>
-    `
-    },
-    {title: "Mics", thehtml: `
-        <h1>Active & Passive</h1>
-        <p>Active is powered and has plugin or battery. Passive does'nt have power source and needs amplifier.</p>
-        <h1>Dynamic & Condenser</h1>
-        <p>...</p>
-    `
-    },
-    {title: "Cables", thehtml: `
-    <img src="./assets/cabletypeslabled.jpg">    
-        <h1>Cable Types</h1>
-        <table>
-        <tr><th>XLR</th><td>"Mic Cable" usually mics and mixer outputs, very strong and durable connection safe from electric distortion (balanced)</td></tr>
-        <tr><th>TS</th><td>"instrument cable"</td></tr>
-        <tr><th>TRS 1/8in</th><td>"Headphone jack"</td></tr>
-        </table>
-        <p>RA stands for Right Angle</p>
-        <h1>Balanced & Unbalanced</h1>
-        <p>...</p>
-    `
-    },
-    {title: "Hand Precussion", thehtml: `
+    {title: "Hand Precussion🪘", thehtml: `
     <img src="./assets/handdrumslabled.jpg">    
     <h1>Conga</h1>
-        <h1>Bongo</h1>
-        <h1>Cajon</h1>
+        <p>Traditional set has 3 congas, Tumba (lowest), Tres dos (middle), Quinto (smallest). Tumba on right, TresDos in middle, and Quinto on left</p>
+    <h1>Bongó</h1>
+        <p>Place between legs holding it with your knees. Small drum on left side. Bongo Del Monte is lower pitch bongo used in cuba with the smaller drum on the right side.</p>
+    <h1>Cajon</h1>
         <p>Cajon means box in spanish. Sit on it and hit the front face. 2 Types: Cajon with snare inside, and cajon without a snare.</p>
-        <h1>Djembe</h1>
+    <h1>Djembé</h1>
         <p>African drum either tightened with rope (traditional) or tightened with a metal frame.</p>
-        <h1>Darbuka</h1>
+    <h1>Darbuka</h1>
         <p>Middle easter belly dancing drum. Made of metal sound has very tinny sound.</p>
         <img src="./assets/smallhandlabled.jpg">    
-        <h1>Clave</h1>
-        <p>...</p>
-        <h1>Guiro</h1>
-        <p>...</p>
+    <h1>Clave</h1>
+        <p>Clave means "key" in spanish. It makes click sound used for special patterns in afro-caribbean music. The instrument is 2 sticks you hit together, but there are also Clave Blocks (the bright color things)</p>
+    <h1>Shakers & Maracas</h1>
+        <p>The bright color LP shakers detach. Colors are different sounds: Yellow (Light), Blue (Medium), Red (Heavy). You can attach different color shakers to get different sounds</p>
+    <h1>Guiro</h1>
+        <p>Wood with grooves, there are also metal ones with little holes</p>
+    <h1>Cowbell (Campana)</h1>
+        <p>
+        Types: Mambo, Cha-cha, Salsa, Timbale<br>
+        Cowbells that don't have the mount/clamp are ment to be held in hand<br>
+        Hit the cowbell with a stick, there are "cowbell beaters" but many people use drumsticks which works fine
+        </p>
+    <h1>Shékere</h1>
+        <p>One hand on bottom, one hand above beads. Shake it!</p>
+    <h1>Cabása</h1>
+        <p>Shake it and hit the beads it with your hand to add accents</p>
+    <h1>Batá</h1>
+        <p>Drum that lays horizontal. Drum head on each side, smaller drum head on your left side.</p>
+    <h1>Timbales</h1>
+        <p>2 Drums played with drum sticks. </p>
+    <h1>Marimbula</h1>
+        <p>A bass/box used in old cuban music,</p>
+    <h1>Segundo Cajon</h1>
+        <p>Standing cajon from Cuba</p>
         <img src="./assets/handdrumtypeslabled.jpg">    
-        <h1>Synthetic & Hide</h1>
+    <h1>Synthetic & Hide</h1>
         <p>Synthetic heads last longer. Hide is brownish and has a spotty texture</p>
-
     `
     },
+    {title: "String Instruments🎸", thehtml: `
+    <h1>Main Types</h1>
+        <p>
+        <b>Electric:</b> plugs into speaker<br>
+        <b>Acoustic:</b> No plugin / pickups<br>
+        <b>Acoustic-Eletric:</b> Acoustic guitar that also can plug in!<br>
+        <b>Fret or Fretless:</b> Frets are the little bars that sparate the fretboard into notes. Fretless instruments allow to play all tones.
+        </p>
+    <h1>Gadgets</h1>
+        <p>
+        Picks: Used to pluck and strum the strings<br>
+        Tuner: Used to tune each string<br>
+        Capo: Clamp on strings to play in different keys (for open chords)<br>
+        Slide: Slide on strings to sound fretless<br>
+        String Winder: <br>
+        Stand: Holds instrument up off the floor. Some stands can hold up to 3 instruments<br>
+        Strap: <br>
+        </p> 
+    <img src="./assets/gadgets.jpg">   
+    <h1>Guitar</h1>
+        <p>
+        Tuning: E A D G B E (Thickest to Thinnest string)<br>
+        Guitars are either steel or nylon string. Majority are steel in the USA, nylon strings are found on flamenco/spanishy and classical guitars which are acoustic.
+        </p>
+    <h1>Bass</h1>
+        <p>
+        Basses are very important in filling in space in music and are often underrated <br>
+        Tuning: E A D G (Same as Guitar, 1 octive below) <br>
+        5 String Tuning: B E A D G
+        </p>
+    <h1>Ukulele</h1>
+        <p>
+        Hawaiian instrument great for music beginers. It's the gateway drug of the music world because many people will try another instrument like guitar after playing for a bit <br>
+        Tuning: G C E A (Thickest to Thifnest string)
+        </p>
+    <h1>Banjo</h1>
+        <p>American folk instrument usually used to fingerpick rhythms</p>
+    <h1>Mandolin</h1>
+        <p>American folk instrument</p>
+    <h1>Pickups</h1>
+        <p>On electric string instruments.<br> 3 Main Types: Single Coil, Humbucker, P90</p>
+        <img src="./assets/pickuptypeslabled.jpg">
+    <h1>World Instruments</h1>
+    <h1>Oud</h1>
+        <p>Fretless middle eastern guitar</p>
+    `
+    },
+    {title: "Brass Instruments🎺", thehtml: `
+    <p>Brass instruments make noise by buzzing your lips into a mouthpiece</p>
+    <h1>Trumpet</h1>
+    <h1>French Horn</h1>
+    <h1>Trombone</h1>
+        <p>Plays notes like a fretless instrument because of the slider. There are 7 standard slide positions
+    <h1>Tuba</h1>
+    <h1>Euphonium</h1>
+    <p>Like a mini tuba</p>
+    `
+    },
+    {title: "Woodwind Instruments🎷", thehtml: `
+    <p>Woodwind Instruments make sound by blowing air into a mouthpiece</p>
+    <h1>Flute</h1>
+    <h1>Clarinet</h1>
+    <h1>Saxophone</h1>
+    <p>Types: Alto, Tenor, Baritone</p>
+    <h1>Oboe</h1>
+    <h1>Bassoon</h1>
+    `
+    },
+    {title: "Mixers🎚️", thehtml: `
+    <h1>Analog & Digital</h1>
+        <p></p>
+    <h1>Volume</h1>
+        <p></p>
+    `
+    },
+    {title: "Speakers🔊", thehtml: `
+    <h1>Active & Passive</h1>
+        <p>Active is powered and has plugin or battery. Passive does'nt have power source and needs amplifier.</p>
+    <h1>Subwoofer "subs"</h1>
+        <p>For the bass and low frequencies. Low frequencies radiate evenly in all directions unlike mid and high range.</p>
+    <h1>Monitor</h1>
+        <p>Smaller speaker that faces towards player to hear themself play.</p>
+    `
+    },
+    {title: "Mics🎤", thehtml: `
+    <h1>Active & Passive</h1>
+        <p>Active is powered and has plugin or battery. Passive does'nt have power source and needs amplifier.</p>
+    <h1>Stage🎤 & Studio🎙️ mics</h1>
+        <p>Stage/Dynamic mics 🎤 are used for live performances because they're durable and reduce feedback. <br> Studio/Condenser mics 🎙️ are used for recording and more sensitive to noise.</p>
+    <h1>Mic Attachments</h1>
+        <p>Pop Filter: reducing the poping puff sound from "p" and "b" usually for studio/condensor mics since they're sensitive. <br> Mic Covers & Muffs: are the fuzzy things that block the wind and breath and keep them clean from dust.</p>
+    `
+    },
+    {title: "Cables🪢", thehtml: `
+    <img src="./assets/cabletypeslabled.jpg">    
+    <h1>Cable Types</h1>
+        <p>
+        <b>XLR:</b> "Mic Cable" usually mics and mixer outputs, very strong and durable connection safe from electric distortion (balanced)<br>
+        <b>TS:</b> "instrument cable"<br>
+        <b>TRS 1/8in:</b> "Headphone jack"<br>
+        <b>RA:</b> stands for Right Angle
+        </p>
+        <p>Everything plugs into the snake input. The snake output plugs into the mixer</p>
+    <h1>Balanced & Unbalanced</h1>
+        <p>...</p>
+    <h1>Adapters</h1>
+        <p>A little device that converts chord tips. Attach the adapter on the tip.</p>
+    <h1>Snake Cables</h1>
+        <p>An extencion chord for all your inputs.</p>
+    <h1>Stage Box</h1>
+        <p>...</p>
+    `
+    },
+    {title: "Other", thehtml: `
+    <h1>Slatwall</h1>
+        <p>Used to hang instruments with ease.</p>
+    `
+    },
+];
+
+const AfroCaribbeanGenresDatabase = [
+    {name: "Salsa", description: `Piano, Bass`},
+    {name: "Merengue", 
+    description: `Fast paced regaton rythem, Strong stomp beat on 1, Merengue Piano Riff, Bass hits on 1`},
+    {name: "Cumbia", description: ``},
+    {name: "Bachata", description: `Thump on down beat, straight beat. Electric Guitar Rhythm. Shaker or Guiro hitting 4th or 8th notes. Bongo hitting rolls, Bass hitting `},
+    {name: "Son", description: `Guitar Rhythm (hit up down up), Bass, Shakers, Tres or double string guitar, `},
+    {name: "Guajira", 
+    description: `1) Piano rhythm, 2) Guiro, 3) bass, 4) bongo & conga`},
+    {name: "Chachacha"},
+    {name: "Bolero", description: `Slow paced and roll on second beat (4bar)`},
+    {name: "Cuban Rumba", description: `Gaguancó is a sub genre of Rumba`},
+    {name: "Samba", description: `1) Guitar finger pick rhythm, 2) Shaker, 3)bass, 4)Hand drum`},
+    {name: "Bosa Nova"},
+    
+    {name: "Calypso"},
+    {name: "Changüí", 
+    description: `Tres/12String rhythm, Shaker (chachacha), bass, clave (2 hits)`},
+];
+// rhythms 
+// bongo (martillo)
+// conga (tumbao)
+// piano (montuno)
+// maraca (chachacha)
+// camana ()
+// clave ()
+// Tresillo, Claves, chachacha
+const FlamencoGenres = [];
+const AmericanGenres = [
+    {name: "Blues"},
+    {name: "Soul"},
+    {name: "Gospel"},
+    {name: "R&B"},
+
+    {name: "Funk"},
+    {name: "Hip-Hop"},
+    {name: "Hip-Hop"},
+
+    {name: "Country"},
+    {name: "Bluegrass"},
+    {name: "Folk"},
+
+    {name: "Rock"},
+    {name: "Metal"},
+];
+const OtherGenres = [
+    {name: "Reggae"}
 ];
 
 function getcurrentmusictopic(){
